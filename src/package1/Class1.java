f8fdff90de923e2a5a8ef29a7b426164079bae99
sueda <suedasogutlu03@gmail.com>
2021-08-13 00:20:05 +0200

    first commit for sueda branch

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -4,8 +4,12 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Here is master branch");
+		
 		System.out.println("Hello world!");
 		
 		System.out.println("Welcome Bank Porject members!");
+	
+		System.out.println("Here is a fourth commit");
 	}
 }
